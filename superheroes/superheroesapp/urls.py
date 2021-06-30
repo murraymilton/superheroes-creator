94e91f6a177a38c8ac2ce19c3afac568a6d49794
murraymilton <murray.l.milton@outlook.com>
2021-06-30 12:22:21 +0200

    created single enpoints within superheroesapp.urls. Created function in superheroesapp/views.py

--- superheroes/superheroesapp/urls.py
+++ superheroes/superheroesapp/urls.py
@@ -1,1 +1,13 @@
-from . import  views
+from . import views
+from django.urls import path
+
+
+app_name = 'superheroesapp'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:superhero_id>/', views.display_data, name='display_data'),
+    path('new/', views.create_new_superhero, name='create_new_superhero'),
+    path('edit/<int:superhero_id>/', views.superhero_edit, name='superhero_edit'),
+    path('update/<int:superhero_id>/', views.update_superhero, name='update_superhero'),
+    path('delete/<int:superhero_id>/', views.delete_superhero, name='delete_superhero')
+]
--- superheroes/superheroesapp/views.py
+++ superheroes/superheroesapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Superhero
+from .forms import superhero_forms
 
 # Create your views here.
