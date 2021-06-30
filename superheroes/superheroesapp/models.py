2bd76e00dc970a2a5b2e3da897ec9572564f3bc0
murraymilton <murray.l.milton@outlook.com>
2021-06-30 11:59:40 +0200

    Created model classes within superheroesapp model.py to inherit from django.db.models.Model class. Migrated model changes and verified in sql.

--- superheroes/superheroesapp/models.py
+++ superheroes/superheroesapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Superhero(models.Model):
+    superhero_name = models.CharField(max_length=50)
+    alter_ego_name = models.CharField(max_length=50)
+    primary_hero_ability = models.CharField(max_length=50)
+    secondary_hero_ability = models.CharField(max_length=50)
+    superhero_catchphrase = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.superhero_name
